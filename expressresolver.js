54b5498cf6472faf117ccd37955a9ef810e8e97e
Mircea Nistor <mirceanis@gmail.com>
2020-03-12 22:06:27 +0100

    bump ethr-did-resolver to 2.2.0

--- expressresolver.js
+++ expressresolver.js
@@ -4,10 +4,32 @@ const ethr = require('ethr-did-resolver')
 const web = require('web-did-resolver')
 const nacl = require('nacl-did')
 
+//this project ID is only useful for ethr-did resolution
+const infuraId = "ec9c99d75b834bac8dd4bfacad8cfdf7"
+
+const providerConfig = {
+  networks: [
+    { name: "mainnet", rpcUrl: `https://mainnet.infura.io/v3/${infuraId}` },
+    { name: "0x1", rpcUrl: `https://mainnet.infura.io/v3/${infuraId}` },
+    { name: "ropsten", rpcUrl: `https://ropsten.infura.io/v3/${infuraId}` },
+    { name: "0x3", rpcUrl: `https://ropsten.infura.io/v3/${infuraId}` },
+    { name: "rinkeby", rpcUrl: `https://rinkeby.infura.io/v3/${infuraId}` },
+    { name: "0x4", rpcUrl: `https://rinkeby.infura.io/v3/${infuraId}` },
+    { name: "goerli", rpcUrl: `https://goerli.infura.io/v3/${infuraId}` },
+    { name: "0x5", rpcUrl: `https://goerli.infura.io/v3/${infuraId}` },
+    { name: "kovan", rpcUrl: `https://kovan.infura.io/v3/${infuraId}` },
+    { name: "0x2a", rpcUrl: `https://kovan.infura.io/v3/${infuraId}` },
+    { name: "rsk", registry: "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b", rpcUrl: "https://did.rsk.co:4444" },
+    { name: "0x1e", registry: "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b", rpcUrl: "https://did.rsk.co:4444" },
+    { name: "rsk:testnet", registry: "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b", rpcUrl: "https://did.testnet.rsk.co:4444" },
+    { name: "0x1f", registry: "0xdca7ef03e98e0dc2b855be647c39abe984fcf21b", rpcUrl: "https://did.testnet.rsk.co:4444" },
+  ]
+}
+
 const resolver = new Resolver({
-    ...ethr.getResolver(),
+    ...ethr.getResolver(providerConfig),
     ...web.getResolver(),
-    https : web.web,  // Override a did method type,
+    https : web.web,
     nacl: nacl.resolver
 })
 
@@ -33,8 +55,10 @@ var server = app.listen(8081, function () {
 })
 
 // Example DIDs
-// did:muport:Qmbrpc3gKtapsL5k6nZuzYvoMQZwMup5qWvss1q4XuaRJd
 // did:ethr:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736
+// did:ethr:mainnet:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736
+// did:ethr:goerli:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736
+// did:ethr:0x2a:0x3b0BC51Ab9De1e5B7B6E34E5b960285805C41736
 // did:web:uport.me
 // did:https:gbugy.is
 // did:nacl:Md8JiMIwsapml_FtQ2ngnGftNP5UmVCAUuhnLyAsPxI
