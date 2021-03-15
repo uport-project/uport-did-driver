6c1cff4c6f46cbb83187e6b60b336e641c6d5f81
Mircea Nistor <mirceanis@gmail.com>
2021-03-16 00:17:08 +0100

    test: test all known networks

--- src/app.js
+++ src/app.js
@@ -1,81 +1,77 @@
-const { Resolver } = require("did-resolver");
-const ethr = require("ethr-did-resolver");
-const web = require("web-did-resolver");
-const nacl = require("nacl-did");
+const { Resolver } = require('did-resolver')
+const ethr = require('ethr-did-resolver')
+const web = require('web-did-resolver')
+const nacl = require('nacl-did')
 
 //this project ID is only useful for ethr-did resolution
-const infuraId = "ec9c99d75b834bac8dd4bfacad8cfdf7";
+const infuraId = 'ec9c99d75b834bac8dd4bfacad8cfdf7'
 
 const providerConfig = {
   infuraProjectId: infuraId,
   networks: [
-    { name: "rsk", chainId: 30, rpcUrl: "https://did.rsk.co:4444" },
+    { name: 'rsk', chainId: 30, rpcUrl: 'https://did.rsk.co:4444' },
     {
-      name: "rsk:testnet",
+      name: 'rsk:testnet',
       chainId: 31,
-      rpcUrl: "https://did.testnet.rsk.co:4444",
+      rpcUrl: 'https://did.testnet.rsk.co:4444'
     },
     {
-      chainId: 246529,
-      rpcUrl: "https://rpc.sigma1.artis.network",
+      chainId: '0x03c301',
+      rpcUrl: 'https://rpc.sigma1.artis.network'
     },
     {
-      chainId: 246785,
-      rpcUrl: "https://rpc.tau1.artis.network",
+      chainId: '0x03c401',
+      rpcUrl: 'https://rpc.tau1.artis.network'
     },
     {
-      name: "matic",
+      name: 'matic',
       chainId: 137,
-      rpcUrl: "https://rpc-mainnet.matic.network",
+      rpcUrl: 'https://rpc-mainnet.matic.network'
     },
     {
-      name: "maticmum",
+      name: 'maticmum',
       chainId: 80001,
-      rpcUrl: "https://rpc-mumbai.matic.today",
-    },
-  ],
-};
+      rpcUrl: 'https://rpc-mumbai.matic.today'
+    }
+  ]
+}
 
 const resolver = new Resolver(
   {
     ...ethr.getResolver(providerConfig),
     ...web.getResolver(),
-    https: web.getResolver().web,
+    https: web.getResolver().web
   },
   {
     legacyResolvers: {
-      nacl: nacl.resolver,
-    },
+      nacl: nacl.resolver
+    }
   }
-);
+)
 
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-app.get("/1.0/identifiers/*", function (req, res) {
-  const url = req.url;
-  const regex = /\/1.0\/identifiers\/(did:.*)/;
-  const did = regex.exec(url)[1];
+app.get('/1.0/identifiers/*', function (req, res) {
+  const url = req.url
+  const regex = /\/1.0\/identifiers\/(did:.*)/
+  const did = regex.exec(url)[1]
 
-  console.log("Resolving DID: " + did);
+  console.log('Resolving DID: ' + did)
 
   resolver
     .resolve(did)
     .then((result) => {
-      res.send(result);
+      res.send(result)
     })
     .catch((err) => {
-      if (
-        err.message.match(
-          /(Unsupported DID method:)|(Invalid DID)|(Not a valid ethr DID:)/
-        )
-      ) {
-        res.status(400).send(err.message);
+      if (err.message.match(/(Unsupported DID method:)|(Invalid DID)|(Not a valid ethr DID:)/)) {
+        res.status(400).send(err.message)
       } else {
-        console.error(err);
-        res.status(500).send(err.message);
+        console.error(err)
+        res.status(500).send(err.message)
       }
-    });
-});
+    })
+})
 
-module.exports = app;
+module.exports = app
